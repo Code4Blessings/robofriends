98d9637f2a3a49ca5cb092576e9a430f45d10f72
Code4Blessings <robincw35@gmail.com>
2018-12-09 02:05:37 -0500

    completed robofriends

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
-  return (
-    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+const Card = (props) => {
+  return(
+    <div className='tc bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${props.id}?200x200`} />
       <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
-  );
+  )
 }
 
 export default Card;
