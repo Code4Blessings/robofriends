608a8c1307850a3b9db45a0413473d465594cc88
Code4Blessings <robincw35@gmail.com>
2018-12-15 14:37:27 -0500

    Updates

---
+++ precache-manifest.2ad085f540288cc94eb20052cafbbd67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "ce36b393dc2c931a4c70",
+    "url": "/robofriends/static/js/main.ce36b393.chunk.js"
+  },
+  {
+    "revision": "7cfa195a3aa294837477",
+    "url": "/robofriends/static/js/1.7cfa195a.chunk.js"
+  },
+  {
+    "revision": "ce36b393dc2c931a4c70",
+    "url": "/robofriends/static/css/main.6133fc9a.chunk.css"
+  },
+  {
+    "revision": "7cfa195a3aa294837477",
+    "url": "/robofriends/static/css/1.e9d293a0.chunk.css"
+  },
+  {
+    "revision": "a29ff720b2a9b51a0a9e993610088c33",
+    "url": "/robofriends/index.html"
+  }
+];
